6932b37fceb7e71c55166e1f116fae2428418699
nikita-bibik <osbib2@gmail.com>
2021-03-24 12:59:37 +0300

    Add cart component

---
+++ src/components/cart/cart.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <h1>Cart</h1>
+      <ul>
+        {Array.from({ length: 5 }, (v, idx) => (
+          <li>Option {idx + 1}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import List from './components/list';
+import Cart from './components/cart';
 
 ReactDOM.render(
   <React.StrictMode>
     <List />
+    <Cart />
   </React.StrictMode>,
   document.getElementById('root'),
 );
