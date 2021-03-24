081b21259aad5e22a3cbab2d6285a2c026350784
nikita-bibik <osbib2@gmail.com>
2021-03-24 17:35:54 +0300

    Add backend part

---
+++ backend/src/test/kotlin/com/nbibik/bookstore/BookstoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nbibik.bookstore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
