66eb63746c28b6e056e6f1f65b87f9b5adc7dfba
nikita-bibik <osbib2@gmail.com>
2021-03-29 11:57:40 +0300

    fix entity saving

--- backend/src/main/kotlin/com/nbibik/bookstore/entity/BaseEntity.kt
+++ backend/src/main/kotlin/com/nbibik/bookstore/entity/BaseEntity.kt
@@ -1,11 +1,14 @@
 package com.nbibik.bookstore.entity
 
 import java.io.Serializable
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.MappedSuperclass
 
 @MappedSuperclass
 abstract class BaseEntity<TID : Serializable> {
-    @get:Id
-    abstract var id: TID
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: TID? = null
 }
--- backend/src/main/kotlin/com/nbibik/bookstore/entity/BookEntity.kt
+++ backend/src/main/kotlin/com/nbibik/bookstore/entity/BookEntity.kt
@@ -1,22 +1,13 @@
 package com.nbibik.bookstore.entity
 
-import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Table
-import kotlin.properties.Delegates
 
 @Entity
 @Table(name = "books")
-class BookEntity : NumberIdentifiedEntity() {
-    @get:Column(nullable = false)
-    lateinit var name: String
-
-    @get:Column(nullable = false)
-    lateinit var author: String
-
-    @get:Column(nullable = false)
-    var year by Delegates.notNull<Short>()
-
-    @get:Column(nullable = false)
-    lateinit var genre: String
-}
+class BookEntity(
+    val name: String,
+    val author: String,
+    val year: Short,
+    val genre: String,
+) : BaseEntity<Long>()
