3d254d8ef75991295a204cce267990579204c4ef
nikita-bibik <osbib2@gmail.com>
2021-03-30 14:17:14 +0300

    Provide the store

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppSettings, { appSettings } from './app-settings';
 import List from './features/books/list';
 import Cart from './features/books/cart';
+import store from '~/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppSettings.Provider value={appSettings}>
-      <List books={[]} />
-      <Cart />
-    </AppSettings.Provider>
+    <Provider store={store}>
+      <AppSettings.Provider value={appSettings}>
+        <List books={[]} />
+        <Cart />
+      </AppSettings.Provider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
