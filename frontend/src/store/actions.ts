eda697041d6f86a292b8d66c2c5556c13c8883ae
nikita-bibik <osbib2@gmail.com>
2021-03-30 09:48:23 +0300

    extract loading

--- frontend/src/store/actions.ts
+++ frontend/src/store/actions.ts
@@ -2,12 +2,25 @@ import { ThunkDispatch } from 'redux-thunk';
 import { Action, Book, BookActions, PagedList, State } from './types';
 import Axios, { AxiosInstance, Canceler } from 'axios';
 
-let cancelFetchBooks: Canceler | undefined | null;
-
 interface PageParams {
   page?: number;
   pageSize?: number;
 }
+
+const loadBookPagedList = (() => {
+  let cancelFetchBooks: Canceler | undefined | null;
+
+  return async (axios: AxiosInstance, pageParams: PageParams): Promise<PagedList<Book>> => {
+    cancelFetchBooks?.();
+    const { token, cancel } = Axios.CancelToken.source();
+    cancelFetchBooks = cancel;
+    try {
+      return await axios.get('books', { cancelToken: token, params: pageParams });
+    } finally {
+      cancelFetchBooks = null;
+    }
+  };
+})();
 
 const isPageOutOfRange = ({ currentPage, totalPages }: PagedList<any>) =>
   currentPage > totalPages - 1;
@@ -16,25 +29,18 @@ export const fetchBooks = (axios: AxiosInstance, pageParams: PageParams) => asyn
   dispatch: ThunkDispatch<State, void, Action>,
   _getState: () => State,
 ) => {
-  cancelFetchBooks?.();
   dispatch({ type: BookActions.FETCH_BOOKS_REQUEST });
-  let payload: PagedList<Book>;
   try {
-    const { token, cancel } = Axios.CancelToken.source();
-    cancelFetchBooks = cancel;
-    payload = await axios.get('books', { cancelToken: token, params: pageParams });
+    const payload = await loadBookPagedList(axios, pageParams);
+    if (isPageOutOfRange(payload)) {
+      await dispatch(fetchBooks(axios, { ...pageParams, page: payload.totalPages - 1 }));
+    } else {
+      dispatch({ type: BookActions.FETCH_BOOKS_SUCCESS, payload });
+    }
   } catch (e) {
     if (!Axios.isCancel(e)) {
       dispatch({ type: BookActions.FETCH_BOOKS_FAILURE });
       throw e;
     }
-    return;
-  } finally {
-    cancelFetchBooks = null;
-  }
-  if (isPageOutOfRange(payload)) {
-    await dispatch(fetchBooks(axios, { ...pageParams, page: payload.totalPages - 1 }));
-  } else {
-    dispatch({ type: BookActions.FETCH_BOOKS_SUCCESS, payload });
   }
 };
