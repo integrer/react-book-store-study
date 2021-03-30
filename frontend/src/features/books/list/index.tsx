948f678d22efa2b88f99ba6034267a81b97cf955
nikita-bibik <osbib2@gmail.com>
2021-03-30 18:30:44 +0300

    add pagination

--- frontend/src/features/books/list/index.tsx
+++ frontend/src/features/books/list/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootDispatch, RootState } from '~/store';
 import AppSettings from '~/app-settings';
+import Pagination from 'react-paginate';
 import Preloader from '~/components/preloader';
 import List from './list';
 import { fetchBooks, cancelFetchBooks } from '../actions';
@@ -24,10 +25,20 @@ const AppSettingsAware = () => {
           }, []);
           if (state.loading) return <Preloader />;
           if (state.error) return <>Some error occurred...</>;
-          const books = state.list.items.map(i => state.items[i]);
+          const { list, items } = state;
+          const books = list.items.map(i => items[i]);
           return (
             <>
               <List books={books} />
+              <Pagination
+                pageRangeDisplayed={5}
+                forcePage={list.currentPage}
+                pageCount={list.totalPages}
+                marginPagesDisplayed={3}
+                onPageChange={({ selected }) => {
+                  dispatch(fetchBooks(appSettings.api(), { page: selected }));
+                }}
+              />
             </>
           );
         };
