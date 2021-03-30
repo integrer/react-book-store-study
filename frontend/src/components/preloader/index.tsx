6f3afeecb2ceb6c1b11640ac720d13863e78c80e
nikita-bibik <osbib2@gmail.com>
2021-03-30 14:08:00 +0300

    add preloader component

---
+++ frontend/src/components/preloader/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles';
+
+export default () => (
+  <div className="preloader">
+    <div className="preloader__inner">
+      <div />
+    </div>
+  </div>
+);
